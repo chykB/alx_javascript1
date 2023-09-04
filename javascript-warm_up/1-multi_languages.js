496a13bf3f00a74650ea83466cf1be8b31fa3b1f
root <chykB>
2023-09-04 16:07:57 -0700

    print out multiple lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 const lineOne = 'C is fun';
 const lineTwo = 'Python is cool';
 const lineThree = 'JavaScript is amazing';
-console.log(lineOne, lineTwo, lineThree);
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);

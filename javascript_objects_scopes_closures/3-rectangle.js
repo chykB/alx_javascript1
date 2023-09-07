ac6435ba3f2097e9a2ae2f02053aaa982ce8f685
ChykB <malikchika86@gmail.com>
2023-09-07 11:43:59 +0100

    Create an instance method called print() that prints the rectangle using the character X

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle{
     }
     print(){
         for (let i = 0; i < this.height; i++){
-            k = '';
+            let k = '';
         }
         for (let j = 0; j < this.width; j++){
             k += 'X';
